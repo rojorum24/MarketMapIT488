2ca9777c97decb7d9304d16f65dc1150076e3d25
rojorum24 <60782127+rojorum24@users.noreply.github.com>
2024-04-30 13:58:23 -0400

    Add files via upload
    MarketMap files

---
+++ AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MarketMap.ViewModels;
+using MarketMap.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MarketMap
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
